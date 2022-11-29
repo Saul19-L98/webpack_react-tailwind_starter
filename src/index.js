7976c7a6ff2f20e85a331171b60878984a8ae229
sallanez <saul.alejandro19@gmail.com>
2022-11-29 13:04:36 -0600

    react config and dependencies added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import './style.css';
+import "./style.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-console.log('Hello world');
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <h1 class="mx-auto mt-24 text-5xl bg-blue-200 w-1/3 h-14 text-center">
+    Hello World
+  </h1>
+);
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,42 +1,56 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	mode: 'development',
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname,'dist'),
-		filename:'bundle.js',
-	},
-	devServer: {
-		watchFiles: ["src/**/*"],
-		port: 3000,
-		open: true,
-		hot: true,
-		compress: true,
-		historyApiFallback: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				include: path.resolve(__dirname, 'src'),
-				use: ['style-loader', 'css-loader', 'postcss-loader'],
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/i,
-				type: "asset/resource",
-				generator:{
-					filename:"assets/img/[name][ext]"
-				}
-			}
-		],
-	},
-	plugins:[
-		new HtmlWebpackPlugin({
-			inject: true,
-			template: './public/index.html',
-			filename: './index.html',
-		})
-	]
-}
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    watchFiles: ["src/**/*"],
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.html$/i,
+        use: { loader: "html-loader" },
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/img/[name][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
